3bf2dd13ed14d2683249c215eb2114588fec51a4
saumyadwiv <132488903+saumyadwiv@users.noreply.github.com>
2025-05-04 16:24:30 +0530

    Create Perfect number.cpp

---
+++ Day 3/Perfect number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+
+        if(num<=1){
+            return false;
+        }
+
+        int sum=0;
+
+        for(int i=1; i<= num-1;i++){
+            if(num% i==0){
+                sum=sum+i;
+            }
+        }
+
+        return sum==num;
+
+    }
+};
